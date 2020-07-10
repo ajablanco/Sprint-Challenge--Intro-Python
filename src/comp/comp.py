a2bf22e354b5109b1b69e1bc753c8dff7db177d2
Aja Blanco <aja@Ajas-MBP.attlocal.net>
2020-07-10 09:22:16 -0700

    latest changes

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -46,7 +46,7 @@ d = [(x.age + 10) for x in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
-# joined to the age with a hyphen, for example "David-31", for all humans.
+# joined to the age with a hyphen, for example "David-31", for all humans.√
 print("Name hyphen age:")
 e = [(x.name + '-' + str(x.age)) for x in humans]
 print(e)
@@ -60,12 +60,12 @@ print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
-# The "humans" list should be unmodified.
+# The "humans" list should be unmodified.√
 print("All names uppercase:")
 g = [Human(x.name.upper(), (x.age + 5)) for x in humans]
 print(g)
 
-# Write a list comprehension that contains the square root of all the ages.
+# Write a list comprehension that contains the square root of all the ages.√
 print("Square root of ages:")
 import math
 h = [math.sqrt(x.age) for x in humans]
